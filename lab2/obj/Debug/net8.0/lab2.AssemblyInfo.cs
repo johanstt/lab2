ccd6ab9b9563438274c1c70029b37f13b6d6feec
johan <black_johan322@proton.me>
2025-10-31 19:03:01 +0300

    имба артифакт новый

--- lab2/obj/Debug/net8.0/lab2.AssemblyInfo.cs
+++ lab2/obj/Debug/net8.0/lab2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ca84a51daf28b3706319b00376b4959cbbd9f33")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
